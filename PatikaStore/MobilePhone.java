d8a341024e2ab715b01ff0c071f5af7155eae412
fersuekinci <88900722+fersuekinci@users.noreply.github.com>
2021-09-25 03:04:10 +0300

    Create MobilePhone.java

---
+++ PatikaStore/MobilePhone.java
@@ -0,0 +1,5 @@
+public class MobilePhone extends Product{
+    public MobilePhone(int id, String pName, double price, double discount, int stock, int screenSize, String color, int ram, int battary, int storage, int memory, Brand brand) {
+        super(id, pName, price, discount, stock, screenSize, color, ram, battary, storage, memory, brand);
+    }
+}
