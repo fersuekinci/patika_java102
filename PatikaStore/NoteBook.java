466471c35a9821c9c1104e71ce65d503a6f1e3a9
fersuekinci <88900722+fersuekinci@users.noreply.github.com>
2021-09-25 03:03:53 +0300

    Create NoteBook.java

---
+++ PatikaStore/NoteBook.java
@@ -0,0 +1,5 @@
+public class NoteBook extends Product{
+    public NoteBook(int id, String pName, double price, double discount, int stock, int screenSize, String color, int ram, int battary, int storage, int memory, Brand brand) {
+        super(id, pName, price, discount, stock, screenSize, color, ram, battary, storage, memory, brand);
+    }
+}
